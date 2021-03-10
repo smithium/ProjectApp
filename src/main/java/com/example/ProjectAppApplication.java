9a22cd7d2a1906649f9dfdcf74b348129f284920
smithium0 <60355148+smithium0@users.noreply.github.com>
2021-03-10 16:44:26 -0500

    commit2

--- src/main/java/com/example/ProjectAppApplication.java
+++ src/main/java/com/example/ProjectAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectAppApplication {
-
+	//this is my file
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectAppApplication.class, args);
 	}
